88356e3e6cb4e39e0765700a78796b04803ea0b4
leandro viveiros <leandro.oliveira2055@outlook.com>
2019-10-03 21:49:57 -0300

    fixed rest maping bug

--- src/main/java/dev/leandro/viveiros/esig/tarefa/controller/TarefaAPIController.java
+++ src/main/java/dev/leandro/viveiros/esig/tarefa/controller/TarefaAPIController.java
@@ -1,28 +1,19 @@
 package dev.leandro.viveiros.esig.tarefa.controller;
 
-import com.sun.faces.action.RequestMapping;
 import dev.leandro.viveiros.esig.tarefa.model.TarefaRestModel;
 import dev.leandro.viveiros.esig.tarefa.service.TarefaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class TarefaAPIController {
 
     @Autowired
     private TarefaService tarefaService;
 
-    @GetMapping
+    @GetMapping("/api")
     TarefaRestModel all() {
         return new TarefaRestModel(tarefaService.findAllWithoutIp());
-    }
-
-    @DeleteMapping("/{id}")
-    void deleteEmployee(@PathVariable Integer id) {
-        tarefaService.delete(id);
     }
 }
